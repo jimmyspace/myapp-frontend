c26f3c6c5971aec6dfa656353f1d6cd0dc1548de
靳力兵 <jimmy@jinlibingdeMacBook-Pro.local>
2018-02-02 23:22:47 +0800

    404图片

--- src/components/notFound.vue
+++ src/components/notFound.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="notFound">
     <div class="title">
-      似乎来到了未知的星球！<router-link to="/">返回地球</router-link>
+      <img src="../assets/images/404.jpg" alt="">
+      <!-- 似乎来到了未知的星球！<router-link to="/">返回地球</router-link> -->
     </div>
   </div>
 </template>
@@ -17,5 +18,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-
+.notFound{
+  position: relative;
+  width: 75em;
+  min-height: 16em;
+  margin: 4.5em auto 0;
+  overflow: hidden;
+  .title{
+    position: relative;
+    padding-top: 5em;
+    text-align: center;
+    line-height: 3em;
+    font-size: 18px;
+    img{
+      opacity: 0.2
+    }
+  }
+}
 </style>
